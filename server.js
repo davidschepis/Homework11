9a75d4d0e60067df4af3090be5a39e0577d76c92
David Schepis <davidschepis@gmail.com>
2022-01-11 22:26:16 -0700

    POST now writes to db file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const db = require("./db/db.json");
 const fs = require("fs");
 const {v4 : uuidv4} = require("uuid");
+const { json } = require("express/lib/response");
 const PORT = 3001;
 const app = express();
 
@@ -12,31 +13,58 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
+//Handles GET /notes route and responds with notes.html
 app.get("/notes", (req, res) => {
     console.info(`${req.method} request received, responding with notes.html`);
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+//Handles GET /api/notes route and responds with db.json file
 app.get("/api/notes", (req, res) => {
     console.info(`${req.method} request received, responding with db.json`);
-    let data = JSON.parse(fs.readFileSync("./db/db.json"));
-    res.send(data);
+    res.json(db);
 });
 
+//Handles POST /api/notes route, saves the request into the database and responds with the new database item
 app.post("/api/notes", (req, res) => {
     console.info(`${req.method} request received, updating db.json`);
-    const {noteTitle, noteText} = req.body;
-    console.info(`Received \nTitle: ${noteTitle}\nNoteText: ${noteText}`);
+    const {title, text} = req.body;
+    console.info(`Received \nTitle: ${title}\nText: ${text}`);
     let id = uuidv4();
     console.info(`Creating unique id: ${id}`);
+    appendToFile(title, text, id);
+    console.info("Append complete!");
     res.send("response");
 });
 
+//Handles GET wildcard, responds with index.html
 app.get("*", (req, res) => {
     console.info(`${req.method} request received and handled at wildcard, responding with index.html`);
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+//Listens on PORT, which is handled by the above
 app.listen(PORT, () => {
     console.log(`Listening at ${PORT}`);
 });
+
+//Helper function to read from the db, append to it, and write it back
+function appendToFile(noteTitle, noteText, id) {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) {
+            console.err(`Error reading from ./db/db.json ${err}`);
+        }
+        const jsonData = JSON.parse(data);
+        const obj = {
+            title: noteTitle,
+            text: noteText,
+            id: id
+        };
+        jsonData.push(obj);
+        fs.writeFile("./db/db.json", JSON.stringify(jsonData), (err) => {
+            if (err) {
+                console.err(`Error writing to ./db/db.json ${err}`);
+            }
+        });
+    });
+}
